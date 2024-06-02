d02cde5f6afba857846fbc1313424c645bcc3811
Leonardo Henrique <leonardohenriquesoarasalvez784@gmail.com>
2024-06-02 14:17:05 -0300

    feat: add reactive forms

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,11 +1,28 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
-export class FormComponent {}
+export class FormComponent {
+  results = [];
+
+  filterForms!: FormGroup;
+
+  constructor() {
+    this.filterForms = new FormGroup({
+      hour: new FormControl('', Validators.required),
+      showClosed: new FormControl(true, Validators.required),
+    });
+  }
+}

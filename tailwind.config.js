5d7246965dffb41828ec66b11c0cfa64484b2440
Leonardo Henrique <leonardohenriquesoarasalvez784@gmail.com>
2024-06-02 12:17:46 -0300

    style: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-grey": "#333333",
+        "light-grey": "#808080",
+        "yellow": "#FFB612",
+        "red": "#dc0a17",
+        "green": "#2FC022",
+      },
+    },
   },
   plugins: [],
 };
